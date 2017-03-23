8d019577781a75b1969323949711c33311b34b78
tcholakov <tcholakov2@gmail.com>
2017-03-23 18:15:44 +0200

    Removed unused AutoMapper Mapper property in BaseController and fixed the Title of the RegisterRestaurant.cshtml page

--- Source/Web/PlacesToEat.Web/Controllers/BaseController.cs
+++ Source/Web/PlacesToEat.Web/Controllers/BaseController.cs
@@ -2,21 +2,10 @@
 {
     using System.Web.Mvc;
 
-    using AutoMapper;
-
-    using Infrastructure.Mapping;
     using Services.Web.Contracts;
 
     public abstract class BaseController : Controller
     {
         public ICacheService Cache { get; set; }
-
-        protected IMapper Mapper
-        {
-            get
-            {
-                return AutoMapperConfig.Configuration.CreateMapper();
-            }
-        }
     }
 }
